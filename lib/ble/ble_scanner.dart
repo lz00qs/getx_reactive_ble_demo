de85e9cd8f808ad62d1a736b86693b8a19d6b2e9
lz00qs <lz00qs@gmail.com>
2023-02-05 18:32:55 +0800

    [Modify] Add filter to BLE Scanner

--- lib/ble/ble_scanner.dart
+++ lib/ble/ble_scanner.dart
@@ -6,6 +6,8 @@ import 'package:meta/meta.dart';
 
 import 'ble_logger.dart';
 
+/// 这个类负责开启和关闭蓝牙扫描器并输出扫描结果
+/// This class is responsible for starting and stopping the BLE scanner and outputting the scan results.
 class BleScanner extends GetxController {
   BleScanner({
     required FlutterReactiveBle ble,
@@ -21,21 +23,33 @@ class BleScanner extends GetxController {
 
   StreamSubscription? _subscription;
 
-  final rxBleScannerState = Rx<BleScannerState>(const BleScannerState(discoveredDevices: [], scanIsInProgress: false));
+  final rxBleScannerState = Rx<BleScannerState>(
+      const BleScannerState(discoveredDevices: [], scanIsInProgress: false));
 
   final _devices = <DiscoveredDevice>[];
 
-  void startScan(List<Uuid> serviceIds) {
+  void startScan(BleScannerFilter filter) {
     _logMessage('Start ble discovery');
     _devices.clear();
     _subscription?.cancel();
-    _subscription =
-        _ble.scanForDevices(withServices: serviceIds).listen((device) {
+    _subscription = _ble
+        .scanForDevices(withServices: filter.serviceId ?? [])
+        .listen((device) {
       final knownDeviceIndex = _devices.indexWhere((d) => d.id == device.id);
+
       if (knownDeviceIndex >= 0) {
         _devices[knownDeviceIndex] = device;
       } else {
-        _devices.add(device);
+        if (device.name
+            .toLowerCase()
+            .contains((filter.name ?? "".toLowerCase()))) {
+          if(device.id
+              .contains((filter.mac ?? ""))) {
+            if (device.rssi >= (filter.rssi ?? -100)) {
+              _devices.add(device);
+            }
+          }
+        }
       }
       _pushState();
     }, onError: (Object e) => _logMessage('Device scan fails with error: $e'));
@@ -59,9 +73,11 @@ class BleScanner extends GetxController {
     _pushState();
   }
 
-  Future<void> streamDispose() async {
+  @override
+  Future<void> onClose() async {
     rxBleScannerState.close();
     await _stateStreamController.close();
+    super.onClose();
   }
 }
 
@@ -74,4 +90,18 @@ class BleScannerState {
 
   final List<DiscoveredDevice> discoveredDevices;
   final bool scanIsInProgress;
+}
+
+class BleScannerFilter {
+  String? name;
+  String? mac;
+  List<Uuid>? serviceId;
+  int? rssi;
+
+  BleScannerFilter({
+    this.name,
+    this.mac,
+    this.serviceId,
+    this.rssi,
+  });
 }
--- lib/ui/pages/ble_dev_page.dart
+++ lib/ui/pages/ble_dev_page.dart
@@ -16,27 +16,42 @@ class BleDevPage extends StatefulWidget {
   BleDevPageState createState() => BleDevPageState();
 }
 
+class _BleScanFilterController extends GetxController {
+  final nameController = TextEditingController();
+  final macController = TextEditingController();
+  final serviceUuidController = TextEditingController();
+
+  final RxInt rssi = (-100).obs;
+  final rxNameText = "".obs;
+  final rxMacText = "".obs;
+  final rxServiceUuidText = "".obs;
+
+  @override
+  void onClose() {
+    nameController.dispose();
+    macController.dispose();
+    serviceUuidController.dispose();
+    super.onClose();
+  }
+}
+
 class BleDevPageState extends State<BleDevPage> {
-  late TextEditingController _uuidController;
+  final _BleScanFilterController _filter = Get.put(_BleScanFilterController());
 
   @override
   void initState() {
     super.initState();
-
-    _uuidController = TextEditingController()
-      ..addListener(() => setState(() {}));
   }
 
   @override
   void dispose() {
     widget.scanner.stopScan();
-    _uuidController.dispose();
-
+    _filter.dispose();
     super.dispose();
   }
 
   bool _isValidUuidInput() {
-    final uuidText = _uuidController.text;
+    final uuidText = _filter.rxServiceUuidText.value;
     if (uuidText.isEmpty) {
       return true;
     } else {
@@ -49,14 +64,27 @@ class BleDevPageState extends State<BleDevPage> {
     }
   }
 
+  bool _isValidMacInput() {
+    if (_filter.rxMacText.value.isEmpty) return true;
+    RegExp macText = RegExp(r"[0-9a-fA-F]{2}(:[0-9a-fA-F]{2}){5}");
+    return macText.hasMatch(_filter.rxMacText.value);
+  }
+
   void _startScanning() {
-    final text = _uuidController.text;
-    widget.scanner
-        .startScan(text.isEmpty ? [] : [Uuid.parse(_uuidController.text)]);
+    final BleScannerFilter filter = BleScannerFilter();
+    filter.name = _filter.rxNameText.value;
+    filter.mac = _filter.rxMacText.value.toUpperCase();
+    filter.serviceId = _filter.rxServiceUuidText.value.isEmpty
+        ? []
+        : [Uuid.parse(_filter.rxServiceUuidText.value)];
+    filter.rssi = _filter.rssi.value;
+    widget.scanner.startScan(filter);
   }
 
   @override
   Widget build(BuildContext context) {
+    RxDouble rssiSliderTemp = (100.0).obs;
+    RxBool filterExpanded = false.obs;
     return Obx(() {
       List<DiscoveredDevice> discoveredDevices = [];
 
@@ -68,103 +96,195 @@ class BleDevPageState extends State<BleDevPage> {
       discoveredDevices.sort((left, right) => right.rssi.compareTo(left.rssi));
 
       return Scaffold(
-        appBar: AppBar(
-          title: const Text('Scan for devices'),
-        ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 16),
-                  const Text('Service UUID (2, 4, 16 bytes):'),
-                  TextField(
-                    controller: _uuidController,
-                    enabled: !(widget
-                        .scanner.rxBleScannerState.value.scanIsInProgress),
-                    decoration: InputDecoration(
-                        errorText:
-                            _uuidController.text.isEmpty || _isValidUuidInput()
-                                ? null
-                                : 'Invalid UUID format'),
-                    autocorrect: false,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            title: const Text('Scan for devices'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Column(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ExpansionPanelList(
+                        expansionCallback: (int index, bool isExpanded) {
+                          filterExpanded.value = !filterExpanded.value &&
+                              !widget.scanner.rxBleScannerState.value
+                                  .scanIsInProgress;
+                        },
+                        children: [
+                          ExpansionPanel(
+                            headerBuilder:
+                                (BuildContext context, bool isExpanded) {
+                              return ListTile(
+                                title: const Text("Filter",
+                                    style: TextStyle(fontSize: 20)),
+                                subtitle: !(widget.scanner.rxBleScannerState
+                                        .value.scanIsInProgress)
+                                    ? null
+                                    : Text(
+                                        'Stop scanning to change filter.\nDevice count: ${discoveredDevices.length}'),
+                              );
+                            },
+                            body: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 16, vertical: 8),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text('Filter by name',
+                                        style: TextStyle(fontSize: 16)),
+                                    TextField(
+                                      controller: _filter.nameController,
+                                      enabled: !(widget
+                                          .scanner
+                                          .rxBleScannerState
+                                          .value
+                                          .scanIsInProgress),
+                                      onChanged: (value) {
+                                        _filter.rxNameText.value =
+                                            _filter.nameController.text;
+                                      },
+                                      autocorrect: false,
+                                    ),
+                                    const SizedBox(height: 16),
+                                    const Text('Filter by MAC address',
+                                        style: TextStyle(fontSize: 16)),
+                                    TextField(
+                                      controller: _filter.macController,
+                                      enabled: !(widget
+                                          .scanner
+                                          .rxBleScannerState
+                                          .value
+                                          .scanIsInProgress),
+                                      onChanged: (value) {
+                                        _filter.rxMacText.value =
+                                            _filter.macController.text;
+                                      },
+                                      decoration: InputDecoration(
+                                          errorText: _filter.rxMacText.value
+                                                      .isEmpty ||
+                                                  _isValidMacInput()
+                                              ? null
+                                              : 'Invalid MAC address format'),
+                                      autocorrect: false,
+                                    ),
+                                    const SizedBox(height: 16),
+                                    const Text('Filter by service UUID',
+                                        style: TextStyle(fontSize: 16)),
+                                    TextField(
+                                      controller: _filter.serviceUuidController,
+                                      enabled: !(widget
+                                          .scanner
+                                          .rxBleScannerState
+                                          .value
+                                          .scanIsInProgress),
+                                      onChanged: (value) {
+                                        _filter.rxServiceUuidText.value =
+                                            _filter.serviceUuidController.text;
+                                      },
+                                      decoration: InputDecoration(
+                                          errorText: _filter.rxServiceUuidText
+                                                      .value.isEmpty ||
+                                                  _isValidUuidInput()
+                                              ? null
+                                              : 'Invalid service UUID format'),
+                                      autocorrect: false,
+                                    ),
+                                    const SizedBox(height: 16),
+                                    Text(
+                                      'Filter by RSSI: ${-rssiSliderTemp.toInt()} dBm',
+                                      style: const TextStyle(fontSize: 16),
+                                    ),
+                                    Slider(
+                                      value: rssiSliderTemp.value,
+                                      onChanged: (data) {
+                                        if (!widget.scanner.rxBleScannerState
+                                            .value.scanIsInProgress) {
+                                          rssiSliderTemp.value = data;
+                                        }
+                                      },
+                                      onChangeStart: (data) {},
+                                      onChangeEnd: (data) {
+                                        if (!widget.scanner.rxBleScannerState
+                                            .value.scanIsInProgress) {
+                                          _filter.rssi.value = -data.toInt();
+                                        }
+                                      },
+                                      min: 40.0,
+                                      max: 100.0,
+                                      divisions: 60,
+                                      label: '${-rssiSliderTemp.toInt()}',
+                                    ),
+                                  ],
+                                )),
+                            isExpanded: filterExpanded.value &&
+                                !widget.scanner.rxBleScannerState.value
+                                    .scanIsInProgress,
+                          ),
+                        ]),
+                    Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            ElevatedButton(
+                              onPressed: !(widget.scanner.rxBleScannerState
+                                          .value.scanIsInProgress) &&
+                                      _isValidUuidInput() &&
+                                      _isValidMacInput()
+                                  ? _startScanning
+                                  : null,
+                              child: const Text('Scan'),
+                            ),
+                            ElevatedButton(
+                              onPressed: (widget.scanner.rxBleScannerState.value
+                                      .scanIsInProgress)
+                                  ? widget.scanner.stopScan
+                                  : null,
+                              child: const Text('Stop'),
+                            ),
+                          ],
+                        )),
+                    SizedBox(
+                      child: SwitchListTile(
+                          title: const Text("Verbose logging"),
+                          value: widget.bleLogger.rxVerboseLogging.value,
+                          onChanged: (_) {
+                            widget.bleLogger.rxVerboseLogging.value =
+                                !widget.bleLogger.rxVerboseLogging.value;
+                            widget.bleLogger.verboseLoggingUpdate();
+                          }),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Flexible(
+                  child: ListView(
                     children: [
-                      ElevatedButton(
-                        onPressed: !(widget.scanner.rxBleScannerState.value
-                                    .scanIsInProgress) &&
-                                _isValidUuidInput()
-                            ? _startScanning
-                            : null,
-                        child: const Text('Scan'),
-                      ),
-                      ElevatedButton(
-                        onPressed: (widget.scanner.rxBleScannerState.value
-                                .scanIsInProgress)
-                            ? widget.scanner.stopScan
-                            : null,
-                        child: const Text('Stop'),
-                      ),
+                      ...(discoveredDevices)
+                          .map(
+                            (device) => ListTile(
+                              title: Text(device.name),
+                              subtitle:
+                                  Text("${device.id}\nRSSI: ${device.rssi}"),
+                              // leading: const BluetoothIcon(),
+                              onTap: () async {
+                                widget.scanner.stopScan();
+                                await Get.toNamed('/bleDeviceDetailPage',
+                                    arguments: device);
+                              },
+                            ),
+                          )
+                          .toList(),
                     ],
                   ),
-                  SizedBox(
-                    child: SwitchListTile(
-                      title: const Text("Verbose logging"),
-                      value: widget.bleLogger.verboseLogging,
-                      onChanged: (_) =>
-                          setState(widget.bleLogger.toggleVerboseLogging),
-                    ),
-                  ),
-                  SizedBox(
-                    child: ListTile(
-                      title: Text(
-                        !(widget.scanner.rxBleScannerState.value
-                                .scanIsInProgress)
-                            ? 'Enter a UUID above and tap start to begin scanning'
-                            : 'Tap a device to connect to it',
-                      ),
-                      trailing: ((widget.scanner.rxBleScannerState.value
-                                  .scanIsInProgress) ||
-                              (widget.scanner.rxBleScannerState.value
-                                      .discoveredDevices)
-                                  .isNotEmpty)
-                          ? Text(
-                              'count: ${discoveredDevices.length}',
-                            )
-                          : null,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-            const SizedBox(height: 8),
-            Flexible(
-              child: ListView(
-                children: [
-                  ...(discoveredDevices)
-                      .map(
-                        (device) => ListTile(
-                          title: Text(device.name),
-                          subtitle: Text("${device.id}\nRSSI: ${device.rssi}"),
-                          // leading: const BluetoothIcon(),
-                          onTap: () async {
-                            widget.scanner.stopScan();
-                            await Get.toNamed('/bleDeviceDetailPage',
-                                arguments: device);
-                          },
-                        ),
-                      )
-                      .toList(),
-                ],
-              ),
-            ),
-          ],
-        ),
-      );
+          ));
     });
   }
 }
